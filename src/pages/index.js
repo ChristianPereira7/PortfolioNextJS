14cf86c2a5a661efb9208166be6d8fe2d631a7e4
Christian Pereira <christianpbsd@gmail.com>
2021-06-22 02:59:24 -0300

    Material UI config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { Button } from '@material-ui/core';
 
 
 export default function Home() {
     return(
-        <div className={styles.container}>
+        <div>
         <Head>
             <title>Tela Incial</title>
         </Head>
 
-        <main className={styles.main}>
-            <h1 className={styles.title}>
+        <main>
+            <h1>
                 Bem vindo ao <a href="http://nextjs.org">site!</a>
+                <br/>
+                <Button variant="contained" color="primary">
+                    Entrar
+              </Button>
             </h1>
         </main>
         </div>
